1a0d30777270cfa7d3df052f3ba72f0dc1b586aa
Armando <flores@mtu.edu>
2015-12-26 15:49:42 -0800

    Styling and fixing carousel and getting waypoints to finally do something regular

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,36 @@ angular.module('armandosApp')
 
 
 .controller('indexCtrl', ['$scope', '$state', function($scope, $state) {
+
+
+    $scope.CarouselDemoCtrl = function(){
+    $scope.myInterval = 4000;
+    $scope.slides = [
+      {
+        image: 'http://lorempixel.com/400/100/',
+        title: 'Sameple Image 1',
+        content: 'dedede lorem lorem lroemeosf seifsoef'
+      },
+      {
+        image: 'http://lorempixel.com/400/500/',
+        title: 'Sameple Image 3',
+        content: 'frfrfrf lorem lorem lroemeosf seifsoef'
+      },
+      {
+        image: 'http://lorempixel.com/400/200/',
+        title: 'Sameple Image 15',
+        content: 'gtggt lorem lorem lroemeosf seifsoef'
+      },
+      {
+        image: 'http://lorempixel.com/400/200/',
+        title: 'Sameple Image 1',
+        content: 'lorempixel lorem lorem lroemeosf seifsoef'
+      }
+    ];
+  }
+
+
+
   // $scope.sticky = 'sticky';
     // This is for waypoints !!!
     // This fires the portfolio view on a long scroll.
@@ -12,11 +42,15 @@ angular.module('armandosApp')
 
     $scope.leavingFold = function() {
        document.getElementById("hero").className = "active";
-
+       document.getElementById("portfolio").className = "active";
     }
-
-  $scope.portfolio = function(){
     // $state.go("portfolio");
+
+  $scope.carouselControls = function(){
+    document.getElementById("carouselHolder").className = "active";
+    console.log('workd')
   }
+
+
   console.log('indexCtrl loaded');
 }]);
